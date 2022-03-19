122b2ab34253d8f6860573da3f28d07932e512b1
Lemoney <railkill@gmail.com>
2022-03-20 02:57:33 +0800

    Fix backtracking NPC spawns

--- neighborhood.gd
+++ neighborhood.gd
@@ -3,7 +3,8 @@ extends Node2D
 
 # Positions where NPC should be.
 var npc_spots = [
-	NPCData.new(Vector2(407, 0))
+	NPCData.new(Vector2(407, 0)),
+	NPCData.new(Vector2(1920, 0))
 ]
 
 onready var camera = $Camera
@@ -15,30 +16,35 @@ onready var start_x = 0
 onready var edge_x = foreground.motion_mirroring.x  # order is important
 
 
+func _ready():
+	pass
+	# Should Neighborhood spawn all NPCs baed on data?
+
+
 func _physics_process(delta):
 	floor_loop.global_position.x = player.global_position.x
 	# Update the position loop.
+	print(start_x - edge_x)
 	var viewport_half_x = get_viewport().size.x / 2
-	if camera.global_position.x + viewport_half_x > start_x + abs(edge_x):
+	if player.global_position.x + viewport_half_x > start_x + edge_x:
 		start_x += edge_x
 		respawn()
-	elif camera.global_position.x - viewport_half_x / 2 < start_x - abs(edge_x) + \
-			viewport_half_x + 200:
+	elif player.global_position.x - viewport_half_x < start_x - edge_x:
 		start_x -= edge_x
-		respawn()
+		respawn(false)
 
 
-func respawn():
+func respawn(ahead = true):
 	# Remove all NPCs who were left behind.
 	for npc in npc_list.get_children():
-		# TODO: Following NPCs should either be moved out of this list,
-		# or have a check so following NPCs don't get destroyed.
-		npc.queue_free()
+		if npc.global_position.distance_to(player.global_position) > edge_x:
+			npc.queue_free()
 	# Respawn NPCs to take on the new loop.
 	for data in npc_spots:
 		var npc = data.npc_scene.instance()
+		var start = start_x if ahead else start_x - edge_x
 		npc_list.add_child(npc)
-		npc.global_position = Vector2(start_x, 510) + data.parallax_offset
+		npc.global_position = Vector2(start, 510) + data.parallax_offset
 		npc.scale = data.scale
 		npc.connect("contact", player, "_on_NPC_contact")
 		npc.connect("contact_loss", player, "_on_NPC_contact_loss")
