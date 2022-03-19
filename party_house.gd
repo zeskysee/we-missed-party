89779cce515bb2338f6f4047cb1c5d271dd4846e
Lemoney <railkill@gmail.com>
2022-03-19 22:28:09 +0800

    Implement magic monster spawn in intro

--- party_house.gd
+++ party_house.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 onready var animation_player = $AnimationPlayer
+onready var magic_monster = $MagicMonster
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,3 +15,7 @@ func _on_test_destroy_timeout():
 	var speech = get_node_or_null("Speech")
 	if is_instance_valid(speech):
 		speech.destroy()
+
+
+func _on_first_speech_tree_exited():
+	magic_monster.appear()
