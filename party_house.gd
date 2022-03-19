96f442af6d930093c8f0e3d3b786c775f77eb443
Lemoney <railkill@gmail.com>
2022-03-19 23:46:34 +0800

    Add transitioning

--- party_house.gd
+++ party_house.gd
@@ -12,10 +12,15 @@ func _ready():
 
 
 func _on_test_destroy_timeout():
-	var speech = get_node_or_null("Speech")
+	var speech = get_node_or_null("FirstSpeech")
 	if is_instance_valid(speech):
 		speech.destroy()
 
 
 func _on_first_speech_tree_exited():
 	magic_monster.appear()
+
+
+func set_ending_text(text):
+	pass
+
---
+++ transition.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+# Transitioner to ensure smooth changing of scenes.
+# The color pallete used for the ColorRects in this scene is from:
+# https://colorhunt.co/palette/557b8339aea9a2d5abe5efc1
+# Really cool stuff.
+
+
+# Just exposes the animation_player finished signal for ease-of-use.
+signal completed(animation_name)
+
+# As usual, the AnimationPlayer to animate the transition.
+onready var animation_player = $AnimationPlayer
+
+
+func _on_animation_finished(anim_name):
+	emit_signal("completed", anim_name)
+
+
+func transition_in():
+	animation_player.play("in")
+
+
+func transition_out():
+	animation_player.play("out")
