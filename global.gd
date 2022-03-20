4f124eeb91291a4b4af5de1b8c9383740bf07526
zeskysee <zeskysee@gmail.com>
2022-03-21 01:22:44 +0800

    Change party title

--- global.gd
+++ global.gd
@@ -10,13 +10,27 @@ var is_ending = false
 # Returns the appropriate ending text based on total number of NPCs.
 func get_ending_text():
 	var text: String
+	
+	if total_number_of_npc >= 19:
+		return "Super-Duper\nWholesome Party"			
+	elif total_number_of_npc >= 9:
+		return "Crazy Wholesome\nParty"
+	
 	match total_number_of_npc:
 		0:
-			text = "Lone"
+			text = "Love Yourself"
 		1:
 			text = "Couple"
 		2:
-			text = "Three-Way"
+			text = "Three-Musketeers"
+		3:
+			text = "Fantastic Four"
+		4:
+			text = "Guardian of Galaxy\nParty"
+		5:
+			text = "Justice League\nParty"
 		_:
 			text = "Wholesome"
+				
+				
 	return "%s\nParty" % text
